a8e5eb68f22e9bda68e694d8ab0b75b28da798bc
daju233 <2212509690@qq.com>
2023-10-23 09:55:31 +0800

    将steamAPI修改为https

--- index/index.js
+++ index/index.js
@@ -1,6 +1,6 @@
 async function getsteam()
 {
-    const data = await fetch ('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=59808D845F65E0DA823A66B9BED4CC25&steamid=76561197960434622&format=json');
+    const data = await fetch ('https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=59808D845F65E0DA823A66B9BED4CC25&steamid=76561197960434622&format=json');
     const mydata= await data.json();
     console.log(mydata);
 }
