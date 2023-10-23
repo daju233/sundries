5e08d817b1260af02b23bc584cc2ea7e5364ef64
daju233 <2212509690@qq.com>
2023-10-23 09:50:41 +0800

    添加steam API

--- index/index.js
+++ index/index.js
@@ -1,1 +1,8 @@
-const head=document.getElementById('xiaoren');
+async function getsteam()
+{
+    const data = await fetch ('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=59808D845F65E0DA823A66B9BED4CC25&steamid=76561197960434622&format=json');
+    const mydata= await data.json();
+    console.log(mydata);
+}
+
+getsteam();
