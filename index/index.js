32dda4cb75910ca30a5aee8b7926f409975e14d2
daju233 <2212509690@qq.com>
2023-10-24 22:08:07 +0800

    增加aplayer

--- index/index.js
+++ index/index.js
@@ -1,17 +1,12 @@
-function jsonp(req) {
-    var script = document.createElement('script');
-    var url = req.url + '&callback=' + req.callback.name;
-    script.src = url;
-    //console.log(req.callback);console.log(req.callback.name);
-    document.getElementsByTagName('head')[0].appendChild(script);
-}//加入一条script标签
-
-function getsteam(res){
-    //console.log("woxihuanni");
-    console(res);
-}//回调函数
-
-jsonp({
-    url : 'https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=59808D845F65E0DA823A66B9BED4CC25&steamid=76561197960434622&format=json',
-    callback : getsteam
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    lrcType:3,
+    audio: [{
+        name: '対象a',
+        artist: ' anNina',
+        url: 'music1.mp3',
+        cover: 'cover.jpg',
+        lrc:'lrc.lrc'
+    }]
 });
+
